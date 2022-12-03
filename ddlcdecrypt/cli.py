c7b5c83d937351c5bfa2fd0f2be54a7a56fd51b1
Nemris <nemris@protonmail.com>
2022-12-03 07:25:48 +0100

    Switch to proper logging

--- ddlcdecrypt/cli.py
+++ ddlcdecrypt/cli.py
@@ -1,20 +1,10 @@
 """ CLI interface and entrypoints. """
 
 import argparse
+import logging
 import pathlib
-import sys
 
 from ddlcdecrypt import crypto
-
-
-def print_warn(msg: str) -> None:
-    """
-    Print a warning message on stderr.
-
-    Args:
-        msg: Message to print.
-    """
-    print(f"Warning: {msg}", file=sys.stderr)
 
 
 def read_args() -> argparse.Namespace:
@@ -46,6 +36,15 @@ def read_args() -> argparse.Namespace:
             action="store_true",
             help="skip verifying the decryption key and decrypt anyway"
     )
+    parser.add_argument(
+            "-v",
+            "--verbose",
+            action="store_const",
+            const=logging.INFO,
+            default=logging.WARNING,
+            help="display verbose information",
+            dest="loglevel"
+    )
 
     return parser.parse_args()
 
@@ -60,7 +59,7 @@ def main(assets: list[pathlib.Path], destdir: pathlib.Path, force_decrypt: bool)
         force_decrypt: Whether to force the decryption process.
     """
     if not destdir.exists():
-        print_warn(f"'{destdir}' is missing, creating now.\n")
+        logging.warning("'%s' not found - creating now.\n", destdir)
         destdir.mkdir()
 
     for encrypted_asset in assets:
@@ -68,20 +67,24 @@ def main(assets: list[pathlib.Path], destdir: pathlib.Path, force_decrypt: bool)
                 not force_decrypt
                 and not crypto.can_key_decrypt_asset(encrypted_asset, crypto.UNITYFS_KEY)
         ):
-            print_warn(f"skipping '{encrypted_asset}': invalid key.\n")
+            logging.warning("'%s': invalid key - skipping.\n", encrypted_asset)
             continue
 
         decrypted_asset = crypto.compose_destination_path(encrypted_asset, destdir)
 
-        print(f"Decrypting '{encrypted_asset}' and storing in '{decrypted_asset}'...")
+        logging.info("'%s' -> '%s'", encrypted_asset, decrypted_asset)
         try:
             crypto.decrypt_file(encrypted_asset, decrypted_asset, crypto.UNITYFS_KEY)
         except OSError as exception:
-            print_warn(f"{exception}\n")
+            logging.error("'%s': %s", exception.filename, exception.strerror)
 
 
 def wrapper() -> None:
     """ Entrypoint for ddlcdecrypt. """
     args = read_args()
+    logging.basicConfig(
+            format="[%(levelname)s] %(message)",
+            level=args.loglevel
+    )
 
     main(args.assets, args.destdir, args.force_decrypt)
